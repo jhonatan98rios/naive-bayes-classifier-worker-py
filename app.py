2645dd8421bf7f3a126119f8b17cd6f6259c239a
Jhonatan Teixeira Rios <jhonatan98rios@gmail.com>
2024-03-31 17:02:05 +0000

    todolist

---
+++ app.py
@@ -0,0 +1,10 @@
+# TODO
+
+#- [ ] Escutar a fila do SQS
+#- [ ] Coletar o payload com o path
+#- [ ] Consumir arquivo CSV do S3
+#- [ ] Treinar o modelo scykitlearn
+#- [ ] Calcular accuracy
+#- [ ] Fazer o upload do arquivo do modelo
+#- [ ] Atualizar o documento no mongodb
+#- [ ] Caso dê erro no treinamento, atualizar o documento com status Failed
